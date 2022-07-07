9cb1f5cbadc7fc3f9b3539a9e92f7a9d357704dc
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2022-07-07 10:49:34 -0600

    Finish section 3

--- react-hooks-intro-2/src/index.js
+++ react-hooks-intro-2/src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './ClassApp';
-import App from './FunctionApp'
+// import App from './FunctionApp'
+// import Login from './Login'
+import Register from './Register'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <Login /> */}
+    <Register />
   </React.StrictMode>
 );
 
